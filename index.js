352f5b416c6716c85395760876e72c8052a17eae
godkate <bogodul883@gmail.com>
2022-10-07 17:15:44 -0400

    Added user number's sum

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-
+const firstNumber = +prompt('Enter your first number');
+const secNumber = +prompt('Enter your second number');
+const sum = `Your sum is ${firstNumber + secNumber}`;
+const result = alert(sum);
 
 
